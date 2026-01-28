9d7fb86565c10507cfa240fc52d9cfa78685a445
eliemine24 <elie.gautier24@gmail.com>
2026-01-28 11:01:44 +0100

    ZEFQSD

---
+++ js_project/src/utils/user_input.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+// Create an interface for input and output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuestion = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+};
